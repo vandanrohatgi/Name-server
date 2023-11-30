5f0b30317fa858bc58f9cc81e25998b5cf504035
Vandan Rohatgi <vandanrohatgi@gmail.com>
2023-11-30 12:34:47 +0530

    feat: add random rick reponse (hah)

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
 
 	"github.com/google/gopacket"
@@ -11,14 +13,17 @@ import (
 
 const (
 	PORT     = 8888
-	IP       = "127.0.0.1"
+	IP       = "0.0.0.0"
 	RICKROLL = "rickroll.it"
-    RESET  = "\033[0m"
-    RED    = "\033[31m"
+	RESET    = "\033[0m"
+	RED      = "\033[31m"
 )
 
 func main() {
+	// print sick ASCII art
 	printASCII()
+
+	// Address of this DNS server
 	laddr := net.UDPAddr{
 		IP:   net.ParseIP(IP),
 		Port: PORT,
@@ -30,18 +35,22 @@ func main() {
 
 	log.Printf("Listening for DNS requests on %s:%d", IP, PORT)
 
+	// listen for DNS requests
 	for {
 		tmp := make([]byte, 1024)
 		_, clientAddr, err := u.ReadFrom(tmp)
 		if err != nil {
 			log.Printf("error reading request: %v", err)
 		}
+		// parse the UDP request
 		packet := gopacket.NewPacket(tmp, layers.LayerTypeDNS, gopacket.Default)
 		dnsPacket := packet.Layer(layers.LayerTypeDNS)
 		tcp, ok := dnsPacket.(*layers.DNS)
 		if !ok {
 			log.Printf("unsupported type packet: %T", dnsPacket)
 		}
+
+		// serve the answer
 		err = serveDNS(u, &clientAddr, tcp)
 		if err != nil {
 			log.Print(err)
@@ -49,17 +58,51 @@ func main() {
 	}
 }
 
+// serveDNS has a 1 in 10 chance to redirect the DNS query to a rickroll. It
+// either acts like any other DNS and replies with an IP or just returns a CNAME
+// to a rickroll.
 func serveDNS(u *net.UDPConn, clientAddr *net.Addr, request *layers.DNS) error {
-	var dnsAnswer layers.DNSResourceRecord
+	var answer, responseType string
+	var err error
 
 	reply := request
 	questionRecord := string(request.Questions[0].Name)
 
-	log.Printf("Resolving %s to %s", questionRecord, RICKROLL)
+	log.Printf("Resolving %s", questionRecord)
 
-	dnsAnswer.Type = layers.DNSTypeCNAME
-	dnsAnswer.CNAME = []byte(RICKROLL)
-	dnsAnswer.Name = []byte(questionRecord)
+	if n := rand.Intn(10); n == 1 {
+		answer = RICKROLL
+		responseType = "CNAME"
+	} else {
+		answer, err = resolveHost(questionRecord)
+		responseType = "A"
+		if err != nil {
+			return err
+		}
+	}
+
+	replyData, err := DNSreply(reply, answer, responseType, questionRecord)
+	if err != nil {
+		return err
+	}
+	u.WriteTo(replyData, *clientAddr)
+	return nil
+}
+
+// DNSreply returns the reply to the query with structured byte data.
+func DNSreply(reply *layers.DNS, response, responseType, question string) ([]byte, error) {
+	var dnsAnswer layers.DNSResourceRecord
+
+	switch responseType {
+	case "A":
+		dnsAnswer.Type = layers.DNSTypeA
+		dnsAnswer.IP = net.ParseIP(response)
+	case "CNAME":
+		dnsAnswer.Type = layers.DNSTypeCNAME
+		dnsAnswer.CNAME = []byte(response)
+	}
+
+	dnsAnswer.Name = []byte(question)
 	dnsAnswer.Class = layers.DNSClassIN
 
 	reply.QR = true
@@ -72,25 +115,36 @@ func serveDNS(u *net.UDPConn, clientAddr *net.Addr, request *layers.DNS) error {
 	buf := gopacket.NewSerializeBuffer()
 	err := reply.SerializeTo(buf, gopacket.SerializeOptions{})
 	if err != nil {
-		return fmt.Errorf("error serializing reply: %v", err)
+		return nil, fmt.Errorf("error serializing reply: %v", err)
 	}
 
-	u.WriteTo(buf.Bytes(), *clientAddr)
-	return nil
+	return buf.Bytes(), nil
 }
 
+// resolveHost uses the system DNS service to repond with actual IP address of
+// the request host.
+func resolveHost(host string) (string, error) {
+	resolver := net.Resolver{}
+	ips, err := resolver.LookupHost(context.Background(), host)
+	if err != nil {
+		return "", fmt.Errorf("error resolving host: ", err)
+	}
+	return ips[0], nil
+}
+
+// sick ASCII
 func printASCII() {
-	fmt.Println(RED+`
+	fmt.Println(RED + `
 
- _______  _        _            _______  _______  _______  ______   _______      _        _______  _______  ______      _________ _______      _______  _        _______             _   
-(  ___  )( \      ( \          (  ____ )(  ___  )(  ___  )(  __  \ (  ____ \    ( \      (  ____ \(  ___  )(  __  \     \__   __/(  ___  )    (  ___  )( (    /|(  ____ \           ( \  
-| (   ) || (      | (          | (    )|| (   ) || (   ) || (  \  )| (    \/    | (      | (    \/| (   ) || (  \  )       ) (   | (   ) |    | (   ) ||  \  ( || (    \/     _      \ \ 
-| (___) || |      | |          | (____)|| |   | || (___) || |   ) || (_____     | |      | (__    | (___) || |   ) |       | |   | |   | |    | |   | ||   \ | || (__        (_)      ) )
-|  ___  || |      | |          |     __)| |   | ||  ___  || |   | |(_____  )    | |      |  __)   |  ___  || |   | |       | |   | |   | |    | |   | || (\ \) ||  __)                | |
-| (   ) || |      | |          | (\ (   | |   | || (   ) || |   ) |      ) |    | |      | (      | (   ) || |   ) |       | |   | |   | |    | |   | || | \   || (           _       ) )
-| )   ( || (____/\| (____/\    | ) \ \__| (___) || )   ( || (__/  )/\____) |    | (____/\| (____/\| )   ( || (__/  )       | |   | (___) |    | (___) || )  \  || (____/\    (_)_    / / 
-|/     \|(_______/(_______/    |/   \__/(_______)|/     \|(______/ \_______)    (_______/(_______/|/     \|(______/        )_(   (_______)    (_______)|/    )_)(_______/      ( )  (_/  
-                                                                                                                                                                               |/        
+ _______  _        _            _______  _______  _______  ______   _______      _        _______  _______  ______      _________ _______      _______ _________ _______  _       
+(  ___  )( \      ( \          (  ____ )(  ___  )(  ___  )(  __  \ (  ____ \    ( \      (  ____ \(  ___  )(  __  \     \__   __/(  ___  )    (  ____ )\__   __/(  ____ \| \    /\
+| (   ) || (      | (          | (    )|| (   ) || (   ) || (  \  )| (    \/    | (      | (    \/| (   ) || (  \  )       ) (   | (   ) |    | (    )|   ) (   | (    \/|  \  / /
+| (___) || |      | |          | (____)|| |   | || (___) || |   ) || (_____     | |      | (__    | (___) || |   ) |       | |   | |   | |    | (____)|   | |   | |      |  (_/ / 
+|  ___  || |      | |          |     __)| |   | ||  ___  || |   | |(_____  )    | |      |  __)   |  ___  || |   | |       | |   | |   | |    |     __)   | |   | |      |   _ (  
+| (   ) || |      | |          | (\ (   | |   | || (   ) || |   ) |      ) |    | |      | (      | (   ) || |   ) |       | |   | |   | |    | (\ (      | |   | |      |  ( \ \ 
+| )   ( || (____/\| (____/\    | ) \ \__| (___) || )   ( || (__/  )/\____) |    | (____/\| (____/\| )   ( || (__/  )       | |   | (___) |    | ) \ \_____) (___| (____/\|  /  \ \
+|/     \|(_______/(_______/    |/   \__/(_______)|/     \|(______/ \_______)    (_______/(_______/|/     \|(______/        )_(   (_______)    |/   \__/\_______/(_______/|_/    \/
+                                                                                                                                                                                  
 
-`+RESET)
+` + RESET)
 }
